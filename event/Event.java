2f22f3752e4a5f1168449ab00b62ee3b55d16a19
Patrick Bradley <patrick_bradley1@yahoo.co.uk>
2017-11-23 20:22:41 +0000

    Add files via upload
    PB

---
+++ event/Event.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package event;
+
+/**
+ *
+ * @author patrickbradley
+ */
+public class Event {
+
+    private double price;
+    private String who;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getWho() {
+        return who;
+    }
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+    
+}
