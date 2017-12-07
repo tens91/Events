ded9f6eac07cb0650d5c9d1a6968a7ccbd1d130a
Tony Ennis <tonyennis91@gmail.com>
2017-12-07 12:23:23 +0000

    Add files via upload

---
+++ FINALPROJEC/EventApp/src/eventapp/Other.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventapp;
+
+/**
+ *
+ * @author aenni
+ */
+public class Other extends Event {
+    
+    String host = "";
+    String specialGuest1 = "";
+    String specialGuest2 = "";
+    String specialGuest3 = "";
+    
+    String name = host +" featuring special guests: " +specialGuest1+", "+specialGuest2+", "+specialGuest3;
+    
+    
+}
---
+++ FINALPROJEC/EventApp/src/eventapp/Show.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventapp;
+
+/**
+ *
+ * @author aenni
+ */
+public class Show extends Event {
+    
+    
+    private String headline = "";
+    private String support = "";
+    
+    private String name = headline+" supported by: "+support;
+    
+    
+    
+    
+}
